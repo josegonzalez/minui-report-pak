69e23ec6bbe01d9196b8b36e91b403ee99879d99
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2025-01-18 13:10:53 -0500

    refactor: write bin/report contents to stdout and fix log message

--- launch.sh
+++ launch.sh
@@ -17,8 +17,8 @@ service_on() {
     fi
 
     show_message "Running $HUMAN_READABLE_NAME" forever
-    PROGDIR="$progdir" "$progdir/bin/report" >"$progdir/log/service.log" 2>&1
-    show_message "Report available at /mnt/SDCARD/report.txt" 2
+    PROGDIR="$progdir" "$progdir/bin/report" >"/mnt/SDCARD/report.log" 2>&1
+    show_message "Report available at /mnt/SDCARD/report.log" 2
 }
 
 service_off() {
